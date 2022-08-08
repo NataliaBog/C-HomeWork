5c5e81f22e1cddfe8844b459d67f8f47baada466
Natalia <natashaaa60802@rambler.ru>
2022-08-08 17:39:13 +0500

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт
+//является ли число чётным
+
+{
+    Console.Write("Введите число");
+}
+double num = Convert.ToInt32(Console.ReadLine());
+
+double rem = num %2;
+
+if (rem == 0) Console.WriteLine(num + "Четное число");
+
+else Console.WriteLine(num + "Не четное число");
