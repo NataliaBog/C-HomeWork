9d36836010513d89739e5ba1cc1297979246a114
Natalia <natashaaa60802@rambler.ru>
2022-08-08 17:49:15 +0500

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Найдем все четные числа от 1 до N. Введите число N");
+
+double N = Convert.ToInt32(Console.ReadLine());
+
+double count = 1, rem = 1;
+
+Console.Write("от 1 to " + N + " четные числа ");
+
+while(count < N + 1)
+{
+    rem = count % 2;
+    if (rem == 0) Console.Write(count + ", ");
+    count = count + 1;
+}
